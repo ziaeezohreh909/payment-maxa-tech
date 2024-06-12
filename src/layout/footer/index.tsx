08d61d2a7a72d4821c41fa81d18e75f2a0d253c2
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-13 01:39:26 +0330

    feat: Build Footer component

---
+++ src/layout/footer/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <Container>
+      <Box sx={{ backgroundColor: "purple" }}>Footer</Box>
+    </Container>
+  );
+}
