8dfd362e70be7a832a798c830aafe5bb8456e9de
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-13 01:17:45 +0330

    feat: Build home services with react query

---
+++ src/components/home/hooks/index.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "react-query";
+import {
+  getBestSellers,
+  getNewProducts,
+  getProductsWithBiggestDiscount,
+} from "../services";
+import { IProduct } from "./types";
+
+export const useGetBestSellers = () => {
+  return useQuery<IProduct[]>({
+    queryKey: ["best-sellers"],
+    queryFn: () => getBestSellers(),
+  });
+};
+
+export const useGetProductsWithBiggestDiscount = () => {
+  return useQuery<IProduct[]>({
+    queryKey: ["biggest-discounts"],
+    queryFn: () => getProductsWithBiggestDiscount(),
+  });
+};
+
+export const useGetNewProducts = () => {
+  return useQuery<IProduct[]>({
+    queryKey: ["new-products"],
+    queryFn: () => getNewProducts(),
+  });
+};
