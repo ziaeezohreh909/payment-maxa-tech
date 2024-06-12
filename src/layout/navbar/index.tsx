4d540f1d33004bd1fa472810f540bb07e5f8ce84
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-13 01:39:50 +0330

    feat: Build Navbar component

---
+++ src/layout/navbar/index.tsx
@@ -0,0 +1,148 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import logo from "../../assets/images/logo.png";
+import { Stack } from "@mui/material";
+import AccountMenu from "./account-menu";
+import CartMenu from "./cart-menu";
+import SearchPopup from "./search-popup";
+import { useAuth } from "@/hooks/useAuth";
+import Login from "./login";
+
+const pages = ["Home", "Products", "Blog", "FAQ", "Contact Us"];
+
+export default function Navbar() {
+  const { loggedInUser } = useAuth();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        bgcolor: "transparent",
+        color: "black",
+        boxShadow: "none",
+        px: "110px",
+        py: "20px",
+        borderBottom: "solid blue 2px",
+      }}
+    >
+      <Container disableGutters maxWidth="xl">
+        <Toolbar disableGutters sx={{ paddingLeft: 0, paddingRight: 0 }}>
+          <Stack
+            sx={{ width: "100%" }}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box
+              component="img"
+              src={logo.src}
+              alt="Logo"
+              sx={{ height: "63px", width: "56px", marginRight: 2 }}
+            />
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography
+                      textAlign="center"
+                      sx={{ color: "black", textTransform: "none" }}
+                    >
+                      {page}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    mx: "24px",
+                    fontSize: "18px",
+                    fontWeight: "light",
+                    color: "black",
+                    display: "block",
+                    textTransform: "none",
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={4}
+              sx={{ flexGrow: 0 }}
+            >
+              <SearchPopup />
+              <CartMenu />
+              {loggedInUser ? <AccountMenu /> : <Login />}
+            </Stack>
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
