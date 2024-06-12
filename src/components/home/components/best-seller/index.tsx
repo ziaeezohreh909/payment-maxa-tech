8deb80664c91bfbbaa8b21444408ceeb258d4282
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-13 01:08:28 +0330

    feat: Build BestSeller component

---
+++ src/components/home/components/best-seller/index.tsx
@@ -0,0 +1,19 @@
+import Card from "@/components/shared/card";
+import CustomContainer from "@/components/shared/custom-container";
+import { Box } from "@mui/material";
+import React from "react";
+import { useGetBestSellers } from "../../hooks";
+
+export default function BestSeller() {
+  const { data } = useGetBestSellers();
+
+  return (
+    <CustomContainer hasLink={true} title="Best Sellers">
+      <Box display={"flex"} gap={"24px"}>
+        {data?.map((item) => (
+          <Card key={item.itemId} cardProps={item} />
+        ))}
+      </Box>
+    </CustomContainer>
+  );
+}
