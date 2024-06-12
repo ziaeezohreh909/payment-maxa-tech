85b307684336c13b156f8c025b135b3219ddc94b
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-13 01:07:42 +0330

    feat: Build Home component

---
+++ src/components/home/components/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box } from "@mui/material";
+import CategoriesBox from "./categories-box";
+import AmazingDiscounts from "./amazing-discounts";
+import NewProducts from "./new-products";
+import Intro from "./intro";
+import BestSeller from "./best-seller";
+import TopBrands from "./top-brands";
+import FirstBannerBox from "./first-banner-box";
+import SecondBannerBox from "./seconsd-banner-box";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        px: "110px",
+      }}
+    >
+      <Intro />
+      <CategoriesBox />
+      <AmazingDiscounts />
+      <NewProducts />
+      <FirstBannerBox />
+      <BestSeller />
+      <TopBrands />
+      <SecondBannerBox />
+    </Box>
+  );
+}
