f85b5a5beba8d2d60e82cd3a88c0b2ceb33585ee
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-14 23:47:55 +0330

    fix: Edit ProductsPage

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,13 @@
+import Products from "@/components/product/components";
+import Layout from "@/layout";
+import React from "react";
+
+function ProductsPage() {
+  return (
+    <Layout>
+      <Products />
+    </Layout>
+  );
+}
+
+export default ProductsPage;
