4ed209b7bdaf83750dddce66362ffa12d3896325
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-15 21:42:27 +0330

    fix: Modify TechnicalDetails component

--- src/components/single-product/components/technical/index.tsx
+++ src/components/single-product/components/technical/index.tsx
@@ -23,24 +23,30 @@ export default function TechnicalDetails({ detail }: { detail: IProduct }) {
         <Table>
           <TableBody>
             <TableRow sx={{ backgroundColor: "#F9F9F9" }}>
-              <TableCell sx={{ color: "#717171" }}>Description</TableCell>
+              <TableCell sx={{ color: "#717171" }}>Intro</TableCell>
               <TableCell sx={{ paddingLeft: "50px" }}>
                 {detail?.shortDescription}
               </TableCell>
             </TableRow>
             <TableRow>
+              <TableCell sx={{ color: "#717171" }}>Description</TableCell>
+              <TableCell sx={{ paddingLeft: "50px" }}>
+                {detail?.longDescription}
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ backgroundColor: "#F9F9F9" }}>
               <TableCell sx={{ color: "#717171" }}>Width</TableCell>
               <TableCell sx={{ paddingLeft: "50px" }}>
                 {detail?.dimensions.width}
               </TableCell>
             </TableRow>
-            <TableRow sx={{ backgroundColor: "#F9F9F9" }}>
+            <TableRow>
               <TableCell sx={{ color: "#717171" }}>Height</TableCell>
               <TableCell sx={{ paddingLeft: "50px" }}>
                 {detail?.dimensions.height}
               </TableCell>
             </TableRow>
-            <TableRow>
+            <TableRow sx={{ backgroundColor: "#F9F9F9" }}>
               <TableCell sx={{ color: "#717171" }}>Depth</TableCell>
               <TableCell sx={{ paddingLeft: "50px" }}>
                 {detail?.dimensions.depth}
