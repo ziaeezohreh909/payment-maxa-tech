c5d4712c557c62285b7b9eddb579a92adcf45d82
zahra <z.peikani23@gmail.com>
2024-06-17 20:10:53 +0330

    contact us page

---
+++ src/pages/contact-us/index.tsx
@@ -0,0 +1,9 @@
+import ContactUs from "@/components/contact-us/index";
+import Layout from "@/layout";
+export default function ContactUsPage() {
+  return (
+    <Layout>
+      <ContactUs />
+    </Layout>
+  );
+}
