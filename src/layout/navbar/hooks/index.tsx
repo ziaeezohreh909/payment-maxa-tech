e85999971c1ebd458d5693694ec3101d12c8635b
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-18 17:15:07 +0330

    fix: Modify useSignUpNewUser hook to include cart and wishlist data

--- src/layout/navbar/hooks/index.tsx
+++ src/layout/navbar/hooks/index.tsx
@@ -1,10 +1,13 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   fetchAccessCookie,
+  getCartItemDetails,
+  getCartItems,
   getUserInfo,
   signInUser,
   signUpNewUser,
 } from "../services";
+import { ICart, IUser, IWishlist } from "./types";
 
 export const useAccessCookie = () => {
   return useQuery({
@@ -25,12 +28,32 @@ export const useGetUserInfo = () => {
 
 export const useSignUpNewUser = () => {
   return useMutation({
-    mutationFn: signUpNewUser,
+    mutationFn: (data: {
+      newUserData: IUser;
+      cartData: ICart;
+      wishlistData: IWishlist;
+    }) => signUpNewUser(data.newUserData, data.cartData, data.wishlistData),
   });
 };
 
 export const useSignInUser = () => {
   return useMutation({
     mutationFn: signInUser,
+  });
+};
+
+export const useGetCartItems = (userId) => {
+  return useQuery({
+    queryKey: ["cartItems", userId],
+    queryFn: () => getCartItems(userId),
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useGetCartItemDetails = (productId: number) => {
+  return useQuery({
+    queryKey: ["cartItemDetails", productId],
+    queryFn: () => getCartItemDetails(productId),
+    refetchOnWindowFocus: false,
   });
 };
