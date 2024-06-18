bdc925d5b71579e26e4814e5745c65d179819ed7
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-18 17:17:42 +0330

    feat: Build FAQPage

---
+++ src/pages/FAQ/index.tsx
@@ -0,0 +1,10 @@
+import FAQ from "@/components/FAQ/components";
+import Layout from "@/layout";
+
+export default function FAQPage() {
+  return (
+    <Layout>
+      <FAQ />
+    </Layout>
+  );
+}
