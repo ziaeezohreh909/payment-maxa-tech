dbab459fbe89b528f4a54bc817286dbdf28da8f8
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-18 17:17:24 +0330

    feat: Build ContactUsPage

---
+++ src/pages/contact-us/index.tsx
@@ -0,0 +1,10 @@
+import ContactUs from "@/components/contact-us/components";
+import Layout from "@/layout";
+
+export default function ContactUsPage() {
+  return (
+    <Layout>
+      <ContactUs />
+    </Layout>
+  );
+}
