62d26e9fa3de64b3d02618bbad46be7888f9814a
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-18 17:17:56 +0330

    feat: Build dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "@/components/dashboard/components";
+import Layout from "@/layout";
+import React from "react";
+
+function DashboardPage() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+}
+
+export default DashboardPage;
